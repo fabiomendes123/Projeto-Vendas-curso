eec6f0777fd6c19cce88e62302b81dd06bb3a0b4
fabiomendes123 <fabiomendes@live.com>
2020-07-07 09:24:48 -0300

    implementing methods delete and update

--- src/br/com/projeto/dao/ClientesDAO.java
+++ src/br/com/projeto/dao/ClientesDAO.java
@@ -23,6 +23,7 @@ public class ClientesDAO {
     // metodo casastrar cliente
     public void cadastrarCliente(Clientes obj){
         try{
+            
             String sql = "insert into tb_clientes(nome,rg,cpf,email,telefone,celular,cep,endereco,numero,complemento,bairro,cidade,estado)"
                     +"values (?,?,?,?,?,?,?,?,?,?,?,?,?)";
             //passo conectar o banco de dados e organizar o comando sql
@@ -52,11 +53,61 @@ public class ClientesDAO {
             JOptionPane.showMessageDialog(null, "Erro"+ erro);
         }
     }
-    public void alaterarCliente(){
+    public void alaterarCliente(Clientes obj){
+        try{
+            
+            String sql = "update tb_clientes set nome=?,rg=?,cpf=?,email=?,telefone=?,celular=?,cep=?,"
+                    + "endereco=?,numero=?,complemento=?,bairro=?,cidade=?,estado=? where id=?";
+                    
+            //passo conectar o banco de dados e organizar o comando sql
+            PreparedStatement stmt=con.prepareStatement(sql);
+            
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getRg());
+            stmt.setString(3, obj.getCpf());
+            stmt.setString(4, obj.getEmail());
+            stmt.setString(5, obj.getTelefone());
+            stmt.setString(6, obj.getCelular());
+            stmt.setString(7, obj.getCep());
+            stmt.setString(8, obj.getEndereco());
+            stmt.setInt(9, obj.getNumero());
+            stmt.setString(10, obj.getComplemento());
+            stmt.setString(11, obj.getBairro());
+            stmt.setString(12, obj.getCidade());
+            stmt.setString(13, obj.getUf());
+            
+            stmt.setInt(14, obj.getId());
+        // executar o comando sql
+            stmt.execute();
+            stmt.close();
+            
+            JOptionPane.showMessageDialog(null, "Alterado com sucesso");
         
+        }
+        catch (SQLException erro){
+            JOptionPane.showMessageDialog(null, "Erro"+ erro);
+        }
     }
-    public void excluirCliente(){
+    public void excluirCliente(Clientes obj){
+       try{
+            
+            String sql = "delete from tb_clientes where id = ?";
+            //passo conectar o banco de dados e organizar o comando sql
+            PreparedStatement stmt=con.prepareStatement(sql);
+            
+            stmt.setInt(1, obj.getId());
+           
+        // executar o comando sql
+            stmt.execute();
+            stmt.close();
+            
+            JOptionPane.showMessageDialog(null, "Excluido com sucesso");
         
+        }
+        catch (SQLException erro){
+            JOptionPane.showMessageDialog(null, "Erro"+ erro);
+        }
+    
     }
     // metodo listar todos os cliente
     
